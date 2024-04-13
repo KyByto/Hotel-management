5d7b730d1f28ef20158ff00b21e8b1338ae340de
Kybyto <136782115+KyByto@users.noreply.github.com>
2024-04-13 14:37:00 +0100

    first commit

--- src/main/java/hotel/Main.java
+++ src/main/java/hotel/Main.java
@@ -8,18 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         DatabaseManager.connectDB();
- Administrateur admin = new Administrateur("LE CHEF", "Otaku");
- Client client = new Client("zaki", "foulardo");
-try {
-    client.DemanderUneReservation("20/03/2023", "14/02/2021");
+ 
 
-    client.DemanderUneReservation("30/04/2023","18/02/2021" );   
-       
-
-}
-catch(Exception e) {
-    System.out.println(e.getMessage());
-}
 
 
 
